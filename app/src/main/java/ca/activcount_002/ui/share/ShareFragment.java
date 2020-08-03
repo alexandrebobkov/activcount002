b78fb1ac8a9d84fe289b732a3f55a07eb1b1ad39
Alexandre B <alexandre.bobkov@gmail.com>
2020-08-02 17:40:04 -0700

    WebView

--- app/src/main/java/ca/activcount_002/ui/share/ShareFragment.java
+++ app/src/main/java/ca/activcount_002/ui/share/ShareFragment.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -17,17 +20,25 @@ import ca.activcount_002.R;
 public class ShareFragment extends Fragment {
 
     private ShareViewModel shareViewModel;
+    private WebSettings webSettings;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
+        //final TextView textView = root.findViewById(R.id.text_share);
+        WebView appWebView = (WebView) root.findViewById(R.id.web_view);
+        appWebView.setWebViewClient(new WebViewClient());
+        appWebView.getSettings().setJavaScriptEnabled(true);
+        appWebView.canGoBack();
+        appWebView.loadUrl("https://www.activcount.ca");
+
+
         shareViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
