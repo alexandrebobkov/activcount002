5b30e1a56639ada7cd2c4f7e32f5198a6f87abb5
Alexandre B <alexandre.bobkov@gmail.com>
2020-08-29 11:24:54 -0700

    webViewApp

--- app/src/main/java/ca/activcount_002/ui/share/ShareFragment.java
+++ app/src/main/java/ca/activcount_002/ui/share/ShareFragment.java
@@ -3,11 +3,14 @@ package ca.activcount_002.ui.share;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
+import android.webkit.WebChromeClient;
+import android.app.ProgressDialog;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -21,6 +24,7 @@ public class ShareFragment extends Fragment {
 
     private ShareViewModel shareViewModel;
     private WebSettings webSettings;
+    private WebView appWebView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,19 +32,45 @@ public class ShareFragment extends Fragment {
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
         //final TextView textView = root.findViewById(R.id.text_share);
-        WebView appWebView = (WebView) root.findViewById(R.id.web_view);
-        appWebView.setWebViewClient(new WebViewClient());
+
+        //final ProgressDialog progressDialog = new ProgressDialog ();
+        //progressDialog.setCancelable(false);
+        appWebView = (WebView) root.findViewById(R.id.web_view);
+
         appWebView.getSettings().setJavaScriptEnabled(true);
-        appWebView.canGoBack();
+        //appWebView.canGoBack();
         appWebView.loadUrl("https://app.activcount.ca/");
 
+        appWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
 
-        shareViewModel.getText().observe(this, new Observer<String>() {
+        appWebView.setWebChromeClient (new WebChromeClient() {
+           public void onProgressChanged(WebView view, int progress) {
+
+           }
+        });
+
+       shareViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s);
+
             }
         });
         return root;
     }
+
+    //@Override
+    public void onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && appWebView.canGoBack()) {
+            appWebView.goBack();
+        }
+    }
+
+
 }
